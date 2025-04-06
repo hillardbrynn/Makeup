3c4cddfb416cc72af772408e803bc10793a6f81c
RCode-9000 <raagul9803@gmail.com>
2025-04-06 16:22:45 -0400

    fixing image error

--- acquired-beauty-app/next.config.ts
+++ acquired-beauty-app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['www.sephora.com'], // Add any other domains you need here too
+  },
 };
 
 export default nextConfig;
